6997e6de48798a50f1c808f4f1c7325d7b3fbac8
meiyunchao <2511633874@qq.com>
2023-11-16 17:57:24 +0800

    'deploy'

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -5,19 +5,14 @@ export default {
       link: "/",
     },
     {
-      text: "Godot 知识体系",
+      text: "手把手教你做《太空射击》",
       collapsible: false,
-      children: [
-        {
-          text: "初识 Godot 引擎",
-          children: ["/3x/quickstart/", "/3x/quickstart/install.md", "/3x/quickstart/project.md", "/3x/quickstart/editor.md"],
-        },
-        { text: "场景系统", children: ["/3x/scene/", "/3x/scene/create-scene.md", "/3x/scene/instantiate-scene.md"] },
-      ],
+      children: ["/3x/quickstart/", "/3x/quickstart/install.md", "/3x/quickstart/project.md", "/3x/quickstart/create-scene.md"],
     },
-    // {
-    //   text: "Godot 游戏复刻",
-    //   collapsible: true,
-    // },
+    {
+      text: "Godot 知识体系",
+      collapsible: true,
+      children: [{ text: "场景系统", children: ["/3x/scene/", "/3x/scene/create-scene.md", "/3x/scene/instantiate-scene.md"] }],
+    },
   ],
 };
