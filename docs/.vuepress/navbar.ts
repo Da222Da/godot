316719c38e6f5a98c2aeadc5c79a0a8697042249
meiyunchao <2511633874@qq.com>
2023-11-29 17:45:20 +0800

    'deploy'

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -4,21 +4,31 @@ export default [
     link: "https://github.com/Da222Da/godot",
   },
   {
-    text: "文档支持",
+    text: "第三方支持",
     children: [
       {
-        text: "VuePress",
-        link: "https://v2.vuepress.vuejs.org/zh/",
+        text: "游戏资源",
+        children: [  
+          {
+            text: "itch",
+            link: "https://itch.io/",
+          }
+        ]
       },
-      { text: "Vue", link: "https://cn.vuejs.org/" },
       {
-        text: "Element-plus",
-        link: "https://element-plus.gitee.io/zh-CN/",
+        text: "文档支持",
+        children: [
+          {
+            text: "VuePress",
+            link: "https://v2.vuepress.vuejs.org/zh/",
+          },
+          { text: "Vue", link: "https://cn.vuejs.org/" },
+          {
+            text: "Element-plus",
+            link: "https://element-plus.gitee.io/zh-CN/",
+          },
+        ]
       },
-      // {
-      //   text: "图标字符库emoji",
-      //   link: "https://www.webfx.com/tools/emoji-cheat-sheet/",
-      // },
     ],
   },
 ];
