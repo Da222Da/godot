8ac74e36a0e92c3fbf8cdad9d460a7046a86fb99
meiyunchao <2511633874@qq.com>
2024-01-11 14:31:59 +0800

    'deploy'

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -1,27 +1,28 @@
 export default {
-    "/": [
-        {
-            text: "简介",
-            link: "/",
-        },
-        { text: "快速上手", link: "/quickstart/" },
-        { text: "场景系统", link: "/scene-system/" },
-        // { text: "2D 图形" },
-        { text: "GDScript 脚本", link: "/gdscript/" },
-        { text: "物理系统", link: "/physic-system/" },
-        // { text: "用户输入" },
-        // { text: "用户界面" },
-        // {
-        //   text: "附录",
-        //   collapsible: true,
-        //   children: ["/appendix/gdscript-grammar.md", "/appendix/project-config.md"],
-        // },
-    ],
-    "/game-copy/": [
-        {
-            text: "简介",
-            link: "/game-copy/",
-        },
-        "/game-copy/space-shooter.md",
-    ],
+  "/": [
+    {
+      text: "简介",
+      link: "/",
+    },
+    { text: "快速上手", link: "/quickstart/" },
+    { text: "场景系统", link: "/scene-system/" },
+    { text: "2D 图形", link: "/2D-graphics/" },
+    { text: "GDScript 脚本", link: "/gdscript/" },
+    { text: "物理系统", link: "/physic-system/" },
+    // { text: "用户输入" },
+    // { text: "用户界面" },
+    // {
+    //   text: "附录",
+    //   collapsible: true,
+    //   children: ["/appendix/gdscript-grammar.md", "/appendix/project-config.md"],
+    // },
+  ],
+  "/game-copy/": [
+    {
+      text: "简介",
+      link: "/game-copy/",
+    },
+    "/game-copy/space-shooter.md",
+    "/game-copy/brave.md",
+  ],
 };
